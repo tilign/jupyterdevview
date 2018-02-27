112493483f5929136b5b76a349eb6b1398af164d
Tiligadas, Nick <nick@tiligadas.net>
2018-02-27 10:26:38 -0500

    Python3 compatibility

--- jupyterdevview.py
+++ jupyterdevview.py
@@ -3,9 +3,10 @@ def _custom_for_suffix(suffix):
     import shutil
     import os
     import sys
-    custom_dir = os.path.join(subprocess.check_output('jupyter --config-dir',shell=True).strip(), 'custom')
-    custom_css_path = os.path.join(custom_dir, 'custom.css')
-    custom_css_sources_path = os.path.join(sys.prefix,'share/jupyterdevview/custom.css')
+    import pathlib2
+    custom_dir = pathlib2.os.path.join(subprocess.check_output('jupyter --config-dir',shell=True).strip(), b'custom')
+    custom_css_path = pathlib2.os.path.join(custom_dir, b'custom.css')
+    custom_css_sources_path = pathlib2.os.path.join(sys.prefix,'share/jupyterdevview/custom.css')
     if not os.path.exists(custom_dir):
         os.makedirs(custom_dir)
     shutil.copy('{}.{}'.format(custom_css_sources_path, suffix), custom_css_path)
