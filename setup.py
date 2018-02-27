886be3cc89eff993e151d58e6d056761fd3ac3b4
Tiligadas, Nick <nick@tiligadas.net>
2018-02-27 11:07:33 -0500

    Bumped version and added dependency to pathlib2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='jupyterdevview',
-    version='1.0.0',
+    version='1.0.1',
     author='Nick Tiligadas',
     author_email='nick@tiligadas.net',
     py_modules=['jupyterdevview'],
@@ -11,5 +11,6 @@ setup(
     license='LICENSE.txt',
     description='Side by side view of code and output in jupyter notebooks',
     long_description=open('README.txt').read(),  
-    data_files=[('share/jupyterdevview',['css/custom.css.dev','css/custom.css.notebook'])]
+    data_files=[('share/jupyterdevview',['css/custom.css.dev','css/custom.css.notebook'])],
+    install_requires=['pathlib2']
 )
